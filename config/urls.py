bbaaa535d237b23d300115d0baa85a3e54c4cd4e
Daniel Andersson <d.andersson86@icloud.com>
2022-09-18 22:38:59 +0200

    Add configuration for urls to the project apps.

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('oauth/', include('social_django.urls')),
+    path('projects/', include('projects.urls')),
+    path('users/', include('users.urls'))
 ]
