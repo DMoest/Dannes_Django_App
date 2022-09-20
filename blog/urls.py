c0f320f1d5c2c4b393ffe10cacdf226a6eec1aa8
Daniel Andersson <d.andersson86@icloud.com>
2022-09-20 19:53:57 +0200

    Add some test views, templats etc. to test restriction of templates depending on user class.

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,11 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.listing, name='listing'),
-    path('detail/<int:pk>/', views.blog_detail, name='detail'),
+    path('', views.blog_listing, name='listing'),
+    path('detail/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+    path('see_request/', views.see_request, name='see_request'),
+    path('user_info/', views.user_info, name='user_info'),
+    path('privet_place/', views.privet_place, name='privet_place'),
+    path('staff_place/', views.staff_place, name='staff_place'),
+    path('add_messages/', views.add_messages, name='add_messages'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+
 from blog.models import Blog
 
 
 # Create your views here.
-def listing(request):
+def blog_listing(request):
     """
     It takes a request, and returns a response
 
@@ -29,3 +32,49 @@ def blog_detail(request, blog_id):
     data = {'blog': blog}
 
     return render(request, 'detail.html', data)
+
+
+def see_request(request):
+    text = f"""
+    Some attributes of the request object:
+    scheme: {request.scheme}
+    path: {request.path}
+    method: {request.method}
+    GET: {request.GET}
+    user: {request.user}
+    """
+
+    return HttpResponse(text, content_type='text/plain')
+
+
+def user_info(request):
+    text = f"""
+    Some user attributes of the request object:
+    username: {request.user.username}
+    is_anonymous: {request.user.is_anonymous}
+    is_authenticated: {request.user.is_authenticated}
+    is_staff: {request.user.is_staff}
+    is_superuser: {request.user.is_superuser}
+    is_active: {request.user.is_active}
+    """
+
+    return HttpResponse(text, content_type='text/plain')
+
+
+@login_required
+def privet_place(request):
+    return HttpResponse('This is a private place', content_type='text/plain')
+
+
+@user_passes_test(lambda user: user.is_staff)
+def staff_place(request):
+    return HttpResponse('User is staff!', content_type='text/plain')
+
+
+@login_required
+def add_messages(request):
+    username = request.user.username
+    messages.add_message(request, messages.INFO, f'Hello {username}')
+    messages.add_message(request, messages.WARNING, f'DANGER will get you {username}!')
+
+    return HttpResponse('Messages Added', content_type='text/plain')
