690c07f0f53e1aa3a3e900757a169eb1483d984e
Daniel Andersson <d.andersson86@icloud.com>
2022-10-06 22:36:23 +0200

    Customize the django admin interface.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from projects.models import Project
+
+
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'short_description', 'description', 'technology', 'link', 'colaborators')
+    list_filter = ('technology', 'image')
+    search_fields = ('id', 'title', 'technology', 'colaborators')
