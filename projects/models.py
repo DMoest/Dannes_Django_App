6b7403b81d953cbdb928536ced9744a68c3104fe
Daniel Andersson <d.andersson86@icloud.com>
2022-10-07 07:12:21 +0200

    Clean up a little in projects.model.

--- projects/models.py
+++ projects/models.py
@@ -8,17 +8,17 @@ from django.db.models import (CharField, Model, TextField)
 
 # Create your models here.
 class Project(Model):
-    title = CharField(max_length=100, blank=False, null=False, default="")
-    description = TextField(blank=False, null=False, default="")
-    short_description = CharField(max_length=255, blank=False, null=False, default="")
-    technology = CharField(max_length=20, blank=True, null=False, default="Django")
-    link = CharField(max_length=255, blank=True, null=False, default="")
-    colaborators = CharField(max_length=255, blank=True, null=False, default="")
+    title = CharField(max_length=100, blank=False, null=False, default='')
+    description = TextField(blank=False, null=False, default='')
+    short_description = CharField(max_length=255, blank=False, null=False, default='')
+    technology = CharField(max_length=20, blank=True, null=False, default='Django')
+    link = CharField(max_length=255, blank=True, null=False, default='')
+    colaborators = CharField(max_length=255, blank=True, null=False, default='')
     image = CharField(max_length=255, blank=True, null=False)
 
     def __str__(self):
         return f"{self.title}"
 
     class Meta:
-        verbose_name_plural = "Projects"
+        verbose_name_plural = 'Projects'
         ordering = ('id', 'title', 'technology')
